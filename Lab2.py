3a50e1c63795fed8eb5d2aec745bfb0d7a293a9e
Jensen <jensenpjy18.24@ichat.sp.edu.sg>
2025-10-28 16:51:57 +0800

    commited lab2

--- Lab2.py
+++ Lab2.py
@@ -8,6 +8,7 @@ def get_user_input():
     num_list=[]
     for value in list:
         num_list.append(float(value))
+    print(num_list)
     return num_list
 
 def calc_average(val):
@@ -22,10 +23,19 @@ def find_min_max(val):
     print([min_num, max_num])
     return [min_num, max_num]
 
-def sort_temperature():
-    print()
-def calc_median_temperature():
-    print()
+def sort_temperature(val):
+    return sorted(val)
+
+def calc_median_temperature(val):
+    x = len(val)%2
+    y= int(len(val)/2)
+    if x == 1:
+        #odd
+        middle = val[y]
+    else:
+        #even
+        middle = (val[y]+val[y-1])/2
+    return middle
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
@@ -33,6 +43,9 @@ def main():
     num_list = get_user_input()
     calc_average(num_list)
     find_min_max(num_list)
-
+    sorted = sort_temperature(num_list)
+    print(sorted)
+    print(calc_median_temperature(sorted))
+    
 if __name__ == "__main__":
     main()
